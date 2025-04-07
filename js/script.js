5074865be900ff552369102b5d9db74a589d3179
LCSW2502 <c0957912@mylambton.ca>
2025-04-07 11:01:41 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*
+Name: Prasanna Banstola
+Student ID: C0957912
+*/
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  hamburger.classList.toggle('active');
+});
